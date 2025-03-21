e9e93db3f7f33b4a32b45c5df3a8e12a85fc71a2
Teja vardhan Vemulapalli <tejavardhan8978@gmail.com>
2025-03-21 17:49:09 -0500

    Second Commit

---
+++ src/Config.java
@@ -0,0 +1,4 @@
+public class Config {
+    public static final String DATA_FILE = "tasks.txt";
+    public static final String APP_NAME = "Simple To-Do List";
+}
---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    public static void printWelcome() {
+        System.out.println("Welcome to the To-Do List Application!");
+    }
+
+    public static void printGoodbye() {
+        System.out.println("Goodbye! Have a productive day!");
+    }
+}
